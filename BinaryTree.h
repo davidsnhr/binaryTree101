f3f179271285ac1333b43fb17f11221753999a9a
David Sánchez <davidsanchez@Davids-MacBook-Pro-3.local>
2024-10-08 18:38:51 -0600

    first commit

---
+++ BinaryTree.h
@@ -0,0 +1,34 @@
+//
+// Created by David Sánchez on 08/10/24.
+//
+
+#ifndef BINARYTREE_H
+#define BINARYTREE_H
+using namespace std;
+
+struct Node {
+    int data;
+    Node* right;
+    Node* left;
+
+    Node(int value);
+};
+
+class BinaryTree {
+public:
+    Node* root;
+
+    //Contructor
+    BinaryTree();
+
+    Node* insert(Node* node, int value);
+    Node* search(Node* node, int value);
+
+
+    // void preorder(Node* node);
+
+};
+
+
+
+#endif //BINARYTREE_H
